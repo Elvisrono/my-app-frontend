9438b7b514ca25bc3c52fc7df791313676370291
¨kericho¨ <lvisrono90@gmail.com>
2023-03-03 16:49:35 +0300

    add a view functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,28 +1,38 @@
-import React , { useEffect , useState} from "react";
+import React, { useEffect, useState } from "react";
 
-function Search({searchFunction}) {
+function Search({ searchFunction, loggedIn}) {
   const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    searchFunction(search)
-  },
-  [search])
-
+  
   // update state on input change 
-  function handleSearch(e){
+  function handleSearch(e) {
     e.preventDefault();
     setSearch(e.target.value);
   }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:9293/users/${loggedIn.id}/tasks`);
+        const data = await response.json();
+        const filteredData = data.filter((item) => item.description.includes(search));
+        searchFunction(filteredData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [search, searchFunction]);
+
   return (
     <form className="d-flex">
-    <input className="formcontrol"
-    type="text" placeholder="Search tasks" onChange={handleSearch}
-    id="search"/>
-    <button className="btn btn-outline-info ms-1"
-        id= "searchBtn">SEARCH</button>
-        
-</form>
-  
+      <input className="formcontrol"
+        type="text"
+        placeholder="Search tasks"
+        onChange={handleSearch}
+        id="search"
+      />
+      <button className="btn btn-outline-info ms-1" id="searchBtn">SEARCH</button>
+    </form>
   );
 }
 
