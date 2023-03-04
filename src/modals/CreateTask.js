2840ddb5d0a85506263149b026088f356ab7fe60
¨kericho¨ <lvisrono90@gmail.com>
2023-03-04 22:56:38 +0300

    rewrite createtask file

--- src/modals/CreateTask.js
+++ src/modals/CreateTask.js
@@ -2,23 +2,48 @@ import React, { useState } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const CreateTask = ({modal, toggle, loggedIn, handleNewPost, handleView}) => {
-    
 
     const [form, setForm] = useState(
         {
             category: "",
             todos: "",
-            dueDate: ""
-            
+            dueDate: "",
+            completed: false
         }
     )
-    
-    // description is category and tasklist is task details
-    
-   
-     //handleSave fcn will push taskObj in taskList array
-     const handleSave =  (e) => {
-        e.preventDefault()
+    const [searchForm, setSearchForm] = useState(
+        {
+            completed: "",
+            dueDate: ""
+        }
+    )
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchParams = Object.keys(searchForm)
+            .filter((key) => searchForm[key])
+            .map((key) => `${key}=${searchForm[key]}`)
+            .join("&");
+
+        fetch(`http://localhost:9293/users/${loggedIn.id}/tasks?${searchParams}`)
+            .then((response) => response.json())
+            .then((data) => setSearchResults(data));
+    };
+
+    const handleReset = (e) => {
+        e.preventDefault();
+        setSearchForm({ completed: "", dueDate: "" });
+        setSearchResults([]);
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setSearchForm({ ...searchForm, [name]: value });
+    };
+
+    const handleSave = (e) => {
+        e.preventDefault();
         fetch(`http://localhost:9293/users/${loggedIn.id}/tasks`, {
             method: 'POST',
             headers: {
@@ -33,42 +58,46 @@ const CreateTask = ({modal, toggle, loggedIn, handleNewPost, handleView}) => {
             setForm({
                 category: "",
                 todos: "",
-                dueDate: ""
+                dueDate: "",
+                completed: false
               });
         })
             
     }
 
- 
     return (
-        
         <Modal isOpen={modal} toggle={toggle}>
             <ModalHeader toggle={toggle}>Create Task - {form.dueDate}</ModalHeader>
-
             <ModalBody>
-            
-                    <form>
-                        <div className='form-group'>
-                            <label>Category</label>
-                            <input type="text" className='form-control' value={form.category} onChange={(e) => setForm({...form, category:e.target.value})} name="taskName"/>
-                        </div>
-                        <div className='form-group'>
-                            <label>Tasks</label>
-                            <textarea rows="5" className='form-control' value={form.todos} onChange={(e) => setForm({...form, todos:e.target.value})} name ="description"></textarea>
-                        </div>
-                        <div className='form-group'>
-                            <label>Due-Date</label>
-                            <input type="date" className='form-control' value={form.dueDate} onChange={(e) => setForm({...form, dueDate:e.target.value})} name="dueDate"></input>
-                        </div>
-                    </form>     
+                <form>
+                    <div className='form-group'>
+                        <label>Category</label>
+                        <input type="text" className='form-control' value={form.category} onChange={(e) => setForm({...form, category:e.target.value})} name="taskName"/>
+                    </div>
+                    <div className='form-group'>
+                        <label>Tasks</label>
+                        <textarea rows="5" className='form-control' value={form.todos} onChange={(e) => setForm({...form, todos:e.target.value})} name ="description"></textarea>
+                    </div>
+                    <div className='form-group'>
+                        <label>Due-Date</label>
+                        <input type="date" className='form-control' value={form.dueDate} onChange={(e) => setForm({...form, dueDate:e.target.value})} name="dueDate"></input>
+                    </div>
+                    <div className='form-group'>
+                        <label>Completed</label>
+                        <select name="completed" className="form-control" onChange={handleInputChange}>
+                            <option value=""></option>
+                            <option value="true">Completed</option>
+                            <option value="false">Not Completed</option>
+                        </select>
+                    </div>
+                </form>
             </ModalBody>
             <ModalFooter>
-            <Button color="primary" onClick={handleSave}>Create</Button>{' '}
-            <Button color="secondary" onClick={toggle}>Cancel</Button>
-             <Button color="info" onClick={handleView}>View</Button>
-            </ModalFooter>
-      </Modal>
-    );
-};
+                <Button color="primary" onClick={handleSave}>Create</Button>{' '}
+                <Button color="secondary" onClick={toggle}>Cancel</Button>
+</ModalFooter>
+</Modal>
+);
+}
 
 export default CreateTask;
